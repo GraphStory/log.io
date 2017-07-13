19544e6afccf2f89a4ca8ed2cd522172e5a073a8
decaffeinate <dominik.freiberger@stylelounge.de>
2017-07-13 11:30:51 +0200

    decaffeinate: Convert log.io-harvester to JS

--- bin/log.js
+++ bin/log.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
-winston = require('winston');
-logging = new winston.Logger({
+/*
+ * decaffeinate suggestions:
+ * DS104: Avoid inline assignments
+ * DS207: Consider shorter variations of null checks
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+let left;
+const winston = require('winston');
+const logging = new winston.Logger({
   transports: [ new winston.transports.Console({
     level: 'error'
   })]
 });
-homeDir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-conf = require(homeDir + '/.log.io/harvester.conf').config;
+const homeDir = process.env[((left = process.platform === 'win32')) != null ? left : {'USERPROFILE' : 'HOME'}];
+const conf = require(homeDir + '/.log.io/harvester.conf').config;
 conf.logging = logging;
-harvester = require('../index.js');
+let harvester = require('../index.js');
 harvester = new harvester.LogHarvester(conf);
 harvester.run();
